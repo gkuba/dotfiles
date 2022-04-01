66512f8c762341a97d9d9a3ea6c21b95f40d2ea0
Geramie Kuba <gkuba@tuxgamer.com>
2022-04-01 03:36:13 -0600

    Fixed variable typo

--- user.sh
+++ user.sh
@@ -49,7 +49,7 @@ done
 
 if ! [ ${#EXISTING_VIMDOTFILES[@]} -eq 0 ]; then
     echo -e "\n$yellow  [INFO]$white Removing vim dotfiles $green\"${EXISTING_VIMDOTFILES[*]}\"$white found in home directory."
-  for file in ${EXISTING_DOTFILES[@]}; do
+  for file in ${EXISTING_VIMDOTFILES[@]}; do
     rm -rf $HOME/.vim/bundle/$file
   done
 fi
