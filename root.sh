bd74bd0a935f5ef3322a95aca18dd9f2ff765580
Geramie Kuba <gkuba@tuxgamer.com>
2022-10-09 14:04:49 -0600

    Removed non dotfile related stuff

--- root.sh
+++ root.sh
@@ -1,13 +1,4 @@
 #! /bin/bash
 
-### TO DO: ###
-# Get this all converted and put in a ansible playbook.
-
-# Install packages
-sudo apt install -y git curl unzip neofetch nala vim most fontconfig zsh zsh-autosuggestions zsh-syntax-highlighting fonts-firacode
-
-# Setup Passwordless sudo
-sed -i 's/^%sudo   ALL=(ALL:ALL) ALL/%sudo   ALL=(ALL) NOPASSWD ALL/' /etc/sudoers
-
 # Install starship
 sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- -y
