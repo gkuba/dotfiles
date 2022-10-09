e02b81fca9bcba16d832570b3d3ee66a208bf823
Geramie Kuba <gkuba@tuxgamer.com>
2022-10-09 12:35:08 -0600

    Adjusted install packages.
    Removed ansible and added neofetch and nala

--- root.sh
+++ root.sh
@@ -4,7 +4,7 @@
 # Get this all converted and put in a ansible playbook.
 
 # Install packages
-sudo apt install -y ansible git curl vim most fontconfig zsh zsh-autosuggestions zsh-syntax-highlighting fonts-firacode
+sudo apt install -y git curl unzip neofetch nala vim most fontconfig zsh zsh-autosuggestions zsh-syntax-highlighting fonts-firacode
 
 # Setup Passwordless sudo
 sed -i 's/^%sudo   ALL=(ALL:ALL) ALL/%sudo   ALL=(ALL) NOPASSWD ALL/' /etc/sudoers
